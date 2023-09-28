fdcbfd1455ebcbefacf63f9c9ff5789f990b65b0
amin <110548277+pogiess@users.noreply.github.com>
2023-09-29 01:36:36 +0330

    EASY-TO-UNDERSTAND SOURCE
    this is a much more simple and easy to understand source code of password generator

--- main.cpp
+++ main.cpp
@@ -3,55 +3,19 @@
 #include <cstdlib>
 #include <ctime>
 
-namespace PasswordUtils {
-
-    /**
-    * @class PasswordGenerator
-    * Generates a random password with specified length.
-    */
-    class PasswordGenerator {
-    private:
-        int length; // Length of the password
-
-    public:
-        /**
-        * Constructs a PasswordGenerator object with the specified length.
-        *
-        * @param len The length of the password to be generated.
-        */
-        PasswordGenerator(int len) {
-            length = len;
-        }
-
-        /**
-        * Generates a random password with the specified length.
-        *
-        * @return std::string The randomly generated password.
-        */
-        std::string GeneratePassword() {
-            std::string password = "";
-            std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*+";
-
-            // Seed the random number generator with the current time
-            std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
-            for (int i = 0; i < length; i++) {
-                int randomIndex = std::rand() % characters.length();
-                password += characters[randomIndex];
-            }
-
-            return password;
-        }
-    };
-}
-
+using namespace std;
 int main() {
-    int passwordLength = 12; // Specify the desired length of the password
+    string password = "";
+    int pass_length = 12;
+    string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*+";
 
-    PasswordUtils::PasswordGenerator passwordGenerator(passwordLength);
-    std::string password = passwordGenerator.GeneratePassword();
+    srand(time(0));
 
-    std::cout << "Generated Password: " << password << std::endl;
+    int index = 0;
+    for (int i = 0; i < pass_length; ++i) {
+        index = rand() % characters.length();
+        password += characters[index];
+    }
 
-    return 0;
+    cout << "generated password : " << password;
 }
